b95ef9dd2054bc002a6c273eeb37ddf344b93157
Priya Murthy <prismm@gmail.com>
2017-05-11 13:02:22 -0400

    working on day panel functionality

--- public/js/trip.js
+++ public/js/trip.js
@@ -15,107 +15,111 @@
  * which take `attraction` objects and pass them to `currentDay`.
  */
 
-var tripModule = (function () {
+var tripModule = (function() {
 
-  // application state
+    // application state
 
-  var days = [],
-      currentDay;
+    var days = [],
+        currentDay;
 
-  // jQuery selections
+    // jQuery selections
 
-  var $addButton, $removeButton;
-  $(function () {
-    $addButton = $('#day-add');
-    $removeButton = $('#day-title > button.remove');
-  });
-
-  // method used both internally and externally
-
-  function switchTo (newCurrentDay) {
-    if (currentDay) currentDay.hide();
-    currentDay = newCurrentDay;
-    currentDay.show();
-  }
-
- // ~~~~~~~~~~~~~~~~~~~~~~~
-    // before calling `addDay` or `deleteCurrentDay` that update the frontend (the UI), we need to make sure that it happened successfully on the server
-  // ~~~~~~~~~~~~~~~~~~~~~~~
-  $(function () {
-    $addButton.on('click', addDay);
-    $removeButton.on('click', deleteCurrentDay);
-  });
-
-
-
-  // ~~~~~~~~~~~~~~~~~~~~~~~
-    // `addDay` may need to take information now that we can persist days -- we want to display what is being sent from the DB
-  // ~~~~~~~~~~~~~~~~~~~~~~~
-  function addDay (day) {
-    var newDay;
-    if (this && this.blur) this.blur(); // removes focus box from buttons
-    if(day) {
-      newDay = day;
-    }else {
-      newDay = dayModule.create({ number: days.length + 1 }); // dayModule
-    }
-    days.push(newDay);
-    if (days.length === 1) {
-      currentDay = newDay;
-    }
-    switchTo(newDay);
-  }
-
-  // ~~~~~~~~~~~~~~~~~~~~~~~
-    // Do not delete a day until it has already been deleted from the DB
-  // ~~~~~~~~~~~~~~~~~~~~~~~
-  function deleteCurrentDay () {
-    // prevent deleting last day
-    if (days.length < 2 || !currentDay) return;
-    // remove from the collection
-    var index = days.indexOf(currentDay),
-      previousDay = days.splice(index, 1)[0],
-      newCurrent = days[index] || days[index - 1];
-    // fix the remaining day numbers
-    days.forEach(function (day, i) {
-      day.setNumber(i + 1);
+    var $addButton, $removeButton;
+    $(function() {
+        $addButton = $('#day-add');
+        $removeButton = $('#day-title > button.remove');
     });
-    switchTo(newCurrent);
-    previousDay.hideButton();
-  }
 
-  // globally accessible module methods
+    // method used both internally and externally
 
-  var publicAPI = {
-
-    load: function () {
-
-      // ~~~~~~~~~~~~~~~~~~~~~~~
-        //If we are trying to load existing Days, then let's make a request to the server for the day. Remember this is async. For each day we get back what do we need to do to it?
-      // ~~~~~~~~~~~~~~~~~~~~~~~
-      $.ajax({
-            method: "GET",
-            url: '/days',
-            async: false
-        })
-        .then((days) => {
-            days.forEach($(addDay));
-        })
-// $(addDay);
-    },
-
-    switchTo: switchTo,
-
-    addToCurrent: function (attraction) {
-      currentDay.addAttraction(attraction);
-    },
-
-    removeFromCurrent: function (attraction) {
-      currentDay.removeAttraction(attraction);
+    function switchTo(newCurrentDay) {
+        if (currentDay) currentDay.hide();
+        currentDay = newCurrentDay;
+        currentDay.show();
     }
 
-  };
+    // ~~~~~~~~~~~~~~~~~~~~~~~
+    // before calling `addDay` or `deleteCurrentDay` that update the frontend (the UI), we need to make sure that it happened successfully on the server
+    // ~~~~~~~~~~~~~~~~~~~~~~~
+    $(function() {
+        $addButton.on('click', addDay);
+        $removeButton.on('click', deleteCurrentDay);
+    });
 
-  return publicAPI;
+
+
+    // ~~~~~~~~~~~~~~~~~~~~~~~
+    // `addDay` may need to take information now that we can persist days -- we want to display what is being sent from the DB
+    // ~~~~~~~~~~~~~~~~~~~~~~~
+    function addDay(day) {
+        var newDay;
+        if (this && this.blur) this.blur(); // removes focus box from buttons
+        if (day) {
+            newDay = dayModule.create({ number: day.number });
+        } else {
+            newDay = dayModule.create({ number: days.length + 1 }); // dayModule
+        }
+        days.push(newDay);
+        if (days.length === 1) {
+            currentDay = newDay;
+        }
+        switchTo(newDay);
+    }
+
+    // ~~~~~~~~~~~~~~~~~~~~~~~
+    // Do not delete a day until it has already been deleted from the DB
+    // ~~~~~~~~~~~~~~~~~~~~~~~
+    function deleteCurrentDay() {
+        // prevent deleting last day
+        if (days.length < 2 || !currentDay) return;
+        // remove from the collection
+        var index = days.indexOf(currentDay),
+            previousDay = days.splice(index, 1)[0],
+            newCurrent = days[index] || days[index - 1];
+        // fix the remaining day numbers
+        days.forEach(function(day, i) {
+            day.setNumber(i + 1);
+        });
+        switchTo(newCurrent);
+        previousDay.hideButton();
+    }
+
+    // globally accessible module methods
+
+    var publicAPI = {
+
+        load: function() {
+
+            // ~~~~~~~~~~~~~~~~~~~~~~~
+            //If we are trying to load existing Days, then let's make a request to the server for the day. Remember this is async. For each day we get back what do we need to do to it?
+            // ~~~~~~~~~~~~~~~~~~~~~~~
+            $.ajax({
+                    method: "GET",
+                    url: '/days'
+                })
+                .then((days) => {
+                    days.sort(function(a, b) {
+                        return a.number - b.number;
+                    });
+                    days.forEach(addDay);
+                    // console.log(days);
+                })
+        },
+
+        switchTo: switchTo,
+
+        gimmeDays: function() { return days },
+
+        addToCurrent: function(attraction) {
+            currentDay.addAttraction(attraction);
+        },
+
+        removeFromCurrent: function(attraction) {
+            currentDay.removeAttraction(attraction);
+        }
+
+    };
+
+    return publicAPI;
 
 }());
